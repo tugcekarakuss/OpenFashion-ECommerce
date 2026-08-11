63b2166f695eef825f673863b4eddc3ea9bec94b
Tuğçe Karakuş <karakustugce1453@gmail.com>
2026-08-12 02:56:54 +0300

    feat: complete mobile navbar functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react"
-import { navbarIcons, menuItems, categories } from "../data/navbar"
+import { navbarIcons, categories } from "../data/navbar"
 
 export default function Navbar() {
-    const [IsOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
+    const [activeCategory, setActiveCategory] = useState("women")
+    const [openItem, setOpenItem] = useState(null)
+    const activeCategoryData = categories.find(
+        category => category.id === activeCategory
+    )
     return (
-        <nav className="max-w-7xl mx-auto py-10 px-3">
+        <nav className="max-w-7xl mx-auto py-10 sticky top-0 px-3 font-tenor-sans">
             {/* mobile menu */}
             <div className="flex items-center justify-between ">
-                <button className="z-50" onClick={() => setIsOpen(val => !val)}>
-                    <img src={IsOpen ? navbarIcons.close : navbarIcons.menu} alt="menu icon" />
+                <button onClick={() => setIsOpen(true)}>
+                    <img src={navbarIcons.menu} alt="menu icon" />
                 </button>
 
                 <a href="">
@@ -21,12 +26,104 @@ export default function Navbar() {
                     <button>
                         <img src={navbarIcons.shoppingBag} alt="shopping icon" />
                     </button>
-
                 </div>
-                
             </div>
 
+            {/* mobile hamburger menu */}
+            {isOpen && (
+                <div className="fixed inset-0 z-40 bg-white text-text-body overflow-y-auto">
+                    <button className="px-3 py-7 mt-5" onClick={() => setIsOpen(false)}>
+                        <img src={navbarIcons.close} alt="menu icon" />
+                    </button>
+                    <div className="flex items-center justify-between border-b-2 border-placeholder/30 px-5 pt-1">
+                        {categories.map((category) => (
+                            <button
+                                key={category.id}
+                                onClick={() => {
+                                    setActiveCategory(category.id)
+                                    setOpenItem(null)
+                                }}
+                                className={`relative pb-4 ${activeCategory === category.id
+                                    ? "text-text-body"
+                                    : "text-text-body/70"
+                                    }`}>
 
+                                <span>{category.label}</span>
+                                {activeCategory === category.id && (
+                                    <>
+                                        <span className="absolute bottom-0 left-1/2 h-0.5 w-20 -translate-x-1/2 bg-secondery" />
+                                        <span className="absolute -bottom-1 left-1/2 h-2 w-2 -translate-x-1/2 rotate-45 bg-primary" />
+                                    </>
+                                )}
+                            </button>
+                        ))}
+                    </div>
+
+                    <div className="px-5 py-6">
+                        {activeCategoryData.items.map(item => (
+                            <div key={item.id}>
+                                {item.children ? (
+                                    <button
+                                        onClick={() => setOpenItem(openItem === item.id ? null : item.id)}
+                                        className="flex w-full items-center justify-between py-3">
+                                        <span>{item.label}</span>
+                                        <img src={openItem === item.id ? navbarIcons.up : navbarIcons.down}
+                                            alt={openItem === item.id ? "close submenu" : "open submenu"} 
+                                        />
+                                    </button>
+                                ) : (
+                                    <a
+                                        href={item.href}
+                                        className="block py-3"
+                                    >
+                                        {item.label}
+                                    </a>
+                                )}
+
+                                {item.children && openItem === item.id && (
+                                    <div className="pl-5">
+                                        {item.children.map(child => (
+                                            <a
+                                                key={child.id}
+                                                href={child.href}
+                                                className="block py-2"
+                                            >
+                                                {child.label}
+                                            </a>
+                                        ))}
+                                    </div>
+                                )}
+
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className=" mt-5 flex flex-col gap-5 px-5 text-text-body/70">
+                        <div className="flex gap-4">
+                            <img src={navbarIcons.phone} alt="phone icon" />
+                            <span>(786) 713-8616</span>
+                        </div>
+                        <div className="flex gap-4">
+                            <img src={navbarIcons.location} alt="location icon" />
+                            <span>Store locator</span>
+                        </div>
+                    </div>
+                    {/* divider */}
+                    <div className="mt-8 flex justify-center">
+                        <div className="relative w-32 border-t border-placeholder">
+                            <span className="absolute left-1/2 top-1/2 h-2 w-2 -translate-x-1/2 -translate-y-1/2 rotate-45 border border-placeholder bg-white" />
+                        </div>
+                    </div>
+
+                    <div className="flex py-10 items-center justify-center gap-9">
+                        <img src={navbarIcons.twitter} alt="twitter icon" />
+                        <img src={navbarIcons.instagram} alt="instagram icon" />
+                        <img src={navbarIcons.youTube} alt="youtube icon" />
+                    </div>
+
+                </div>
+
+            )}
         </nav>
     )
 }
--- src/data/navbar.js
+++ src/data/navbar.js
@@ -6,6 +6,10 @@ import Phone from "../assets/icons/Call.svg";
 import Location from "../assets/icons/Location.svg";
 import Logo from "../assets/icons/Logo.svg";
 import Down from "../assets/icons/Down.svg";
+import Up from "../assets/icons/Up.svg";
+import Instagram from "../assets/icons/Instagram.svg";
+import Twitter from "../assets/icons/Twitter.svg";
+import YouTube from "../assets/icons/YouTube.svg";
 
 export const navbarIcons = {
   menu: Menu,
@@ -16,6 +20,10 @@ export const navbarIcons = {
   location: Location,
   logo: Logo,
   down: Down,
+  up: Up,
+  twitter: Twitter,
+  instagram: Instagram,
+  youTube: YouTube
 };
 
 export const categories = [
@@ -47,7 +55,7 @@ export const categories = [
           },
           {
             id: "blouse-shirt",
-            label: "Blouse/Shirt",
+            label: "Blouse / Shirt",
             href: "#",
           },
           {
@@ -75,32 +83,110 @@ export const categories = [
             label: "Denim",
             href: "#",
           },
+        ],
+      },
+      {
+        id: "bags",
+        label: "Bags",
+        href: "#",
+        children: [
           {
-            id: "kids",
-            label: "Kids",
+            id: "shoulder-bags",
+            label: "Shoulder Bags",
+            href: "#",
+          },
+          {
+            id: "handbags",
+            label: "Handbags",
+            href: "#",
+          },
+          {
+            id: "crossbody-bags",
+            label: "Crossbody Bags",
+            href: "#",
+          },
+          {
+            id: "clutches",
+            label: "Clutches",
             href: "#",
           },
         ],
       },
       {
-        id: "bag",
-        label: "Bag",
-        href: "#",
-      },
-      {
         id: "shoes",
         label: "Shoes",
         href: "#",
+        children: [
+          {
+            id: "heels",
+            label: "Heels",
+            href: "#",
+          },
+          {
+            id: "flats",
+            label: "Flats",
+            href: "#",
+          },
+          {
+            id: "boots",
+            label: "Boots",
+            href: "#",
+          },
+          {
+            id: "sneakers",
+            label: "Sneakers",
+            href: "#",
+          },
+        ],
       },
       {
         id: "beauty",
         label: "Beauty",
         href: "#",
+        children: [
+          {
+            id: "makeup",
+            label: "Makeup",
+            href: "#",
+          },
+          {
+            id: "skincare",
+            label: "Skincare",
+            href: "#",
+          },
+          {
+            id: "fragrance",
+            label: "Fragrance",
+            href: "#",
+          },
+        ],
       },
       {
         id: "accessories",
         label: "Accessories",
         href: "#",
+        children: [
+          {
+            id: "jewelry",
+            label: "Jewelry",
+            href: "#",
+          },
+          {
+            id: "belts",
+            label: "Belts",
+            href: "#",
+          },
+          {
+            id: "sunglasses",
+            label: "Sunglasses",
+            href: "#",
+          },
+          {
+            id: "hats",
+            label: "Hats",
+            href: "#",
+          },
+        ],
       },
     ],
   },
@@ -117,31 +203,95 @@ export const categories = [
         href: "#",
       },
       {
-        id: "apparel",
-        label: "Apparel",
+        id: "clothing",
+        label: "Clothing",
         href: "#",
         children: [
-          {
-            id: "outer",
-            label: "Outer",
-            href: "#",
-          },
           {
             id: "shirts",
             label: "Shirts",
             href: "#",
           },
+          {
+            id: "t-shirts",
+            label: "T-Shirts",
+            href: "#",
+          },
+          {
+            id: "jackets",
+            label: "Jackets",
+            href: "#",
+          },
+          {
+            id: "coats",
+            label: "Coats",
+            href: "#",
+          },
+          {
+            id: "knitwear",
+            label: "Knitwear",
+            href: "#",
+          },
+          {
+            id: "trousers",
+            label: "Trousers",
+            href: "#",
+          },
+          {
+            id: "denim",
+            label: "Denim",
+            href: "#",
+          },
         ],
-      },
-      {
-        id: "bag",
-        label: "Bag",
-        href: "#",
       },
       {
         id: "shoes",
         label: "Shoes",
         href: "#",
+        children: [
+          {
+            id: "sneakers",
+            label: "Sneakers",
+            href: "#",
+          },
+          {
+            id: "boots",
+            label: "Boots",
+            href: "#",
+          },
+          {
+            id: "loafers",
+            label: "Loafers",
+            href: "#",
+          },
+        ],
+      },
+      {
+        id: "bags",
+        label: "Bags",
+        href: "#",
+      },
+      {
+        id: "accessories",
+        label: "Accessories",
+        href: "#",
+        children: [
+          {
+            id: "belts",
+            label: "Belts",
+            href: "#",
+          },
+          {
+            id: "wallets",
+            label: "Wallets",
+            href: "#",
+          },
+          {
+            id: "sunglasses",
+            label: "Sunglasses",
+            href: "#",
+          },
+        ],
       },
     ],
   },
@@ -158,13 +308,84 @@ export const categories = [
         href: "#",
       },
       {
-        id: "apparel",
-        label: "Apparel",
+        id: "girls",
+        label: "Girls",
+        href: "#",
+        children: [
+          {
+            id: "girls-dresses",
+            label: "Dresses",
+            href: "#",
+          },
+          {
+            id: "girls-tops",
+            label: "Tops",
+            href: "#",
+          },
+          {
+            id: "girls-bottoms",
+            label: "Bottoms",
+            href: "#",
+          },
+          {
+            id: "girls-shoes",
+            label: "Shoes",
+            href: "#",
+          },
+        ],
+      },
+      {
+        id: "boys",
+        label: "Boys",
+        href: "#",
+        children: [
+          {
+            id: "boys-tops",
+            label: "Tops",
+            href: "#",
+          },
+          {
+            id: "boys-bottoms",
+            label: "Bottoms",
+            href: "#",
+          },
+          {
+            id: "boys-jackets",
+            label: "Jackets",
+            href: "#",
+          },
+          {
+            id: "boys-shoes",
+            label: "Shoes",
+            href: "#",
+          },
+        ],
+      },
+      {
+        id: "baby",
+        label: "Baby",
+        href: "#",
+        children: [
+          {
+            id: "baby-clothing",
+            label: "Clothing",
+            href: "#",
+          },
+          {
+            id: "baby-accessories",
+            label: "Accessories",
+            href: "#",
+          },
+        ],
+      },
+      {
+        id: "kids-shoes",
+        label: "Shoes",
         href: "#",
       },
       {
-        id: "shoes",
-        label: "Shoes",
+        id: "kids-accessories",
+        label: "Accessories",
         href: "#",
       },
     ],
